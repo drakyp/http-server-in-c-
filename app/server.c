f977607de3573f978e5eefa78a77fc7b2377ff3b
William Lam <william.lam@epita.fr>
2024-07-30 12:41:52 +0200

    codecrafters submit [skip ci]

--- app/server.c
+++ app/server.c
@@ -13,7 +13,7 @@
 int compress_gzip(const char *src, size_t src_len, char *dest, size_t *dest_len) {
     z_stream zs;
     memset(&zs, 0, sizeof(zs));
-    
+
     if (deflateInit2(&zs, Z_DEFAULT_COMPRESSION, Z_DEFLATED, 31, 8, Z_DEFAULT_STRATEGY) != Z_OK) {
         return -1;
     }
@@ -117,8 +117,8 @@ int main(int argc, char *argv[]) {
             //check if it is a GET request
             if (!strncmp(readpath, "GET", strlen("GET"))){
 
-            
-            readpath = strtok(NULL, " ");
+
+                readpath = strtok(NULL, " ");
 
                 //parsing for the user-agent part 
                 char *user_agent = readpath;
@@ -142,13 +142,18 @@ int main(int argc, char *argv[]) {
 
                     // advance the readpath for the thing to echo
                     readpath = readpath + strlen("/echo/");
-
+                    
+                    //retriving the content length and creating a buffer to stock 
                     int cont_len = strlen(readpath);
                     char resp_send[1024];
 
+
+                    //initialize pointer to check if it is the good method to compress
                     char *check_accept;
                     char *accept_encoding = NULL;
                     char *accept = NULL;
+
+                    //checking if the method to compress is right 
                     while ((check_accept = strtok(NULL, "\r\n")) != NULL) {
                         if (!strncmp(check_accept, "Accept-Encoding: ", strlen("Accept-Encoding: "))) {
                             accept_encoding = check_accept + strlen("Accept-Encoding: ");
@@ -156,6 +161,8 @@ int main(int argc, char *argv[]) {
                         }
                     }
 
+
+                    //checking for the method if there is multiple method 
                     int ok_gzip = 0;
                     if (accept_encoding){
                         //Parse the list of encoding method 
@@ -169,23 +176,26 @@ int main(int argc, char *argv[]) {
                         }
                     } 
 
-                    // creating a buffer that will be used for snprintf
+                    //if it is ok we continue
                     if (ok_gzip) {
 
+                        //creating the buffer to parse 
                         char compress[1024];
                         size_t compress_len = sizeof(compress);
+
+                        //applying the method to compress the data 
                         if( !compress_gzip(readpath, cont_len, compress, &compress_len)){
 
-                        
-                        // using snprintf(buf, max, "%s\n"%s) to try to print and save the different information
-                        snprintf(resp_send, sizeof(resp_send), 
-                                 "HTTP/1.1 200 OK\r\n"
-                                 "Content-Type: text/plain\r\n"
-                                 "Content-Encoding: gzip\r\n"
-                                 "Content-Length: %zu\r\n\r\n", 
-                                 compress_len);
+
+                            // using snprintf(buf, max, "%s\n"%s) to try to print and save the different information
+                            snprintf(resp_send, sizeof(resp_send), 
+                                     "HTTP/1.1 200 OK\r\n"
+                                     "Content-Type: text/plain\r\n"
+                                     "Content-Encoding: gzip\r\n"
+                                     "Content-Length: %zu\r\n\r\n", 
+                                     compress_len);
                             send(fd, resp_send, strlen(resp_send), 0);
-                send(fd, compress, compress_len, 0);
+                            send(fd, compress, compress_len, 0);
                         }
                     } else {
                         // using snprintf(buf, max, "%s\n"%s) to try to print and save the different information
@@ -198,88 +208,88 @@ int main(int argc, char *argv[]) {
 
                     byte_sent = send(fd, resp_send, strlen(resp_send), 0);
 
-            }
+                }
 
-            //handling the user-agent case 
-            else if (!strncmp(readpath, "/user-agent", strlen("/user-agent"))){
-                user_agent = strtok(NULL, "\r\n");
-                user_agent = strtok(NULL, "\r\n");
-                user_agent = strtok(NULL, "\r\n");
+                //handling the user-agent case 
+                else if (!strncmp(readpath, "/user-agent", strlen("/user-agent"))){
+                    user_agent = strtok(NULL, "\r\n");
+                    user_agent = strtok(NULL, "\r\n");
+                    user_agent = strtok(NULL, "\r\n");
 
-                user_agent = user_agent + strlen("User-Agent: ");
-                int user_len = strlen(user_agent);
+                    user_agent = user_agent + strlen("User-Agent: ");
+                    int user_len = strlen(user_agent);
 
-                char user_rep[1024];
+                    char user_rep[1024];
 
-                snprintf(user_rep, sizeof(user_rep), "HTTP/1.1 200 OK\r\n" "Content-Type: text/plain\r\n" "Content-Length: %d\r\n\r\n%s", user_len, user_agent );
-
-
-
-                byte_sent = send(fd, user_rep, strlen(user_rep), 0);
-            }
+                    snprintf(user_rep, sizeof(user_rep), "HTTP/1.1 200 OK\r\n" "Content-Type: text/plain\r\n" "Content-Length: %d\r\n\r\n%s", user_len, user_agent );
 
 
-            //handling the return of a files
-            else if (!strncmp(readpath, "/files", strlen("/files")))
-            {
-                // Check for --directory flag and get the directory path
-                const char *directory = NULL;
-                if (argc == 3 && strcmp(argv[1], "--directory") == 0) {
-                    directory = argv[2];
-                } else {
-                    fprintf(stderr, "Usage: %s --directory <path>\n", argv[0]);
-                    return 1;
+
+                    byte_sent = send(fd, user_rep, strlen(user_rep), 0);
                 }
 
 
-                char filepath[1024];
-                snprintf(filepath, sizeof(filepath), "%s/%s", directory, readpath + strlen("/files/"));
-
-
-                //need to be careful when giving the filepath and not the readpath 
-                
-
-                //check if the file exist and is readable 
-                if(access(filepath, R_OK) == 0)
+                //handling the return of a files
+                else if (!strncmp(readpath, "/files", strlen("/files")))
                 {
-                    //opening the file and reading it 
-                    FILE *file = fopen(filepath, "r");
-                    if (file == NULL) 
-                        perror("Error opening file");
-
-                    // Determine the file size
-                    fseek(file, 0, SEEK_END);// move the pointer at the end of the file to know how many byte are in the file 
-                    size_t size = ftell(file);// tell the current position of the file pointer 
-                    fseek(file, 0, SEEK_SET);// put the file pointer at the start as it was
-
-
-                   //buffer to read what is in the file, reading what is in the file and putting it into the buffer 
-                    char rep[1024];
-
-                    //putting what is in the file into the buffer
-                    size_t byteRead = fread( rep, 1, sizeof(rep), file );
-                    fclose(file);
-
-
-                    //checking if we could read the file 
-                    if( byteRead > 0){
-                    //creating a buffer for the response to send into the right format
-                    char rep_send[1024];
-
-                    //trying to printf and putting everything together    
-                    snprintf(rep_send, sizeof(rep_send) ,  "HTTP/1.1 200 OK\r\n" "Content-Type: application/octet-stream\r\n" "Content-Length: %zu\r\n\r\n%s",size, rep );
-
-
-                    byte_sent = send(fd, rep_send, strlen(rep_send), 0);
+                    // Check for --directory flag and get the directory path
+                    const char *directory = NULL;
+                    if (argc == 3 && strcmp(argv[1], "--directory") == 0) {
+                        directory = argv[2];
+                    } else {
+                        fprintf(stderr, "Usage: %s --directory <path>\n", argv[0]);
+                        return 1;
                     }
 
-                }
+
+                    char filepath[1024];
+                    snprintf(filepath, sizeof(filepath), "%s/%s", directory, readpath + strlen("/files/"));
 
 
-                //given by chatgpt
+                    //need to be careful when giving the filepath and not the readpath 
 
 
-/*              // Check for --directory flag and get the directory path
+                    //check if the file exist and is readable 
+                    if(access(filepath, R_OK) == 0)
+                    {
+                        //opening the file and reading it 
+                        FILE *file = fopen(filepath, "r");
+                        if (file == NULL) 
+                            perror("Error opening file");
+
+                        // Determine the file size
+                        fseek(file, 0, SEEK_END);// move the pointer at the end of the file to know how many byte are in the file 
+                        size_t size = ftell(file);// tell the current position of the file pointer 
+                        fseek(file, 0, SEEK_SET);// put the file pointer at the start as it was
+
+
+                        //buffer to read what is in the file, reading what is in the file and putting it into the buffer 
+                        char rep[1024];
+
+                        //putting what is in the file into the buffer
+                        size_t byteRead = fread( rep, 1, sizeof(rep), file );
+                        fclose(file);
+
+
+                        //checking if we could read the file 
+                        if( byteRead > 0){
+                            //creating a buffer for the response to send into the right format
+                            char rep_send[1024];
+
+                            //trying to printf and putting everything together    
+                            snprintf(rep_send, sizeof(rep_send) ,  "HTTP/1.1 200 OK\r\n" "Content-Type: application/octet-stream\r\n" "Content-Length: %zu\r\n\r\n%s",size, rep );
+
+
+                            byte_sent = send(fd, rep_send, strlen(rep_send), 0);
+                        }
+
+                    }
+
+
+                    //given by chatgpt
+
+
+                    /*              // Check for --directory flag and get the directory path
                 const char *directory = NULL;
                 if (argc == 3 && strcmp(argv[1], "--directory") == 0) {
                     directory = argv[2];
@@ -332,22 +342,22 @@ int main(int argc, char *argv[]) {
                     }
                     free(rep);
                 }*/
-                else {
-                    char *resp_error = "HTTP/1.1 404 Not Found\r\n\r\n";
+                    else {
+                        char *resp_error = "HTTP/1.1 404 Not Found\r\n\r\n";
 
-                    byte_sent = send(fd, resp_error, strlen(resp_error), 0 );
+                        byte_sent = send(fd, resp_error, strlen(resp_error), 0 );
 
+
+                    }
 
                 }
 
-            }
+                //in case nothing is found return error 404
+                else{
+                    char *resp_error = "HTTP/1.1 404 Not Found\r\n\r\n";
 
-            //in case nothing is found return error 404
-            else{
-                char *resp_error = "HTTP/1.1 404 Not Found\r\n\r\n";
-
-                byte_sent = send(fd, resp_error, strlen(resp_error), 0 );
-            }
+                    byte_sent = send(fd, resp_error, strlen(resp_error), 0 );
+                }
             }
             else{
 
@@ -366,9 +376,9 @@ int main(int argc, char *argv[]) {
 
                 char filepath[1024];
                 snprintf(filepath, sizeof(filepath), "%s/%s", directory, filename);
-                
-                 /*
-         
+
+                /*
+
                 //retrieving the number of character needed 
                 readpath = strtok(NULL, "\r\n");
                 readpath = strtok(NULL, "\r\n");
@@ -397,8 +407,8 @@ int main(int argc, char *argv[]) {
                 int byte_sent = send(fd, rep_post, strlen(rep_post), 0);
 
 
-                   
-                
+
+
 
 
 
